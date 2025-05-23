af83acb22a4d272280f402eb2fca115505ec77c1
yhfay <yhfay@ULTIMATE>
2025-05-23 18:27:17 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -242,4 +242,4 @@ def handle_register(data):
 
 # 启动应用
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0', debug=True)                                             
